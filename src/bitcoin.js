3683861782e75bd08c009f22c67becf9bfd4773e
Matt Bell <mappum@gmail.com>
2017-02-25 14:19:29 -0800

    Added blockr.io API client

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -1,4 +1,5 @@
 const bs58check = require('bs58check')
+const request = require('request')
 const { sha2, ripemd160 } = require('./hash.js')
 const { concat, byte } = require('./util.js')
 
@@ -9,6 +10,25 @@ function getAddress (pub, testnet = false) {
   return bs58check.encode(payload)
 }
 
+function blockrRequest (url, { testnet }, cb) {
+  let network = testnet ? 'tbtc' : 'btc'
+  request({
+    url: `https://${network}.blockr.io/api/v1/${url}`,
+    json: true
+  }, (err, res, body) => {
+    if (err) return cb(err)
+    if (body.status !== 'success') {
+      return cb(Error(body.message))
+    }
+    cb(null, body.data)
+  })
+}
+
+function fetchTransactions (address, opts, cb) {
+  blockrRequest(`address/txs/${address}`, opts, cb)
+}
+
 module.exports = {
-  getAddress
+  getAddress,
+  fetchTransactions
 }
