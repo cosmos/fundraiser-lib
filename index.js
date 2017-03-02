0c234870bb1fbddd78a62f18ff0cabb7a8ecc0bc
Matt Bell <mappum@gmail.com>
2017-03-01 22:18:45 -0800

    Set up babel builds

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = Object.assign(
   {
-    bitcoin: require('./src/bitcoin.js'),
-    Client: require('./src/backups.js')
+    bitcoin: require('./lib/bitcoin.js'),
+    Client: require('./lib/backups.js')
   },
-  require('./src/wallet.js')
+  require('./lib/wallet.js')
 )
