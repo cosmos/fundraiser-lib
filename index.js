01f63f51fc2cce374a96337ae0a8eea0839450b5
Matt Bell <mappum@gmail.com>
2017-03-01 16:46:55 -0800

    Added REST client for backup server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = Object.assign(
   {
-    bitcoin: require('./src/bitcoin.js')
+    bitcoin: require('./src/bitcoin.js'),
+    Client: require('./src/backups.js')
   },
   require('./src/wallet.js')
 )
---
+++ src/backups.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const request = require('request')
+const old = require('old')
+
+// client for the fundraiser REST API, for backing up wallets
+class Client {
+  constructor (uri) {
+    this.uri = uri
+  }
+
+  request (method, url, data, cb) {
+    request({
+      method,
+      url: this.uri + url,
+      json: data || true,
+      jar: true // keep cookies
+    }, (err, res, data) => {
+      if (err) return cb(err)
+      if (res.statusCode !== 200) {
+        let error = String(data.error || data)
+        return cb(Error(`${res.statusCode} ${error}`))
+      }
+      cb(null, data)
+    })
+  }
+  get (url, cb) { this.request('GET', url, null, cb) }
+  post (url, data, cb) { this.request('POST', url, data, cb) }
+
+  register (user, cb) { this.post('/register', user, cb) }
+  login (user, cb) { this.post('/login', user, cb) }
+  logout (cb) { this.post('/logout', null, cb) }
+  getWallets (cb) { this.get('/wallets', cb) }
+  getTransactions (cb) { this.get('/transactions', cb) }
+  backupWallet (wallet, cb) {
+    wallet = {
+      encryptedSeed: wallet.encryptedSeed.toString('base64'),
+      salt: wallet.salt.toString('base64'),
+      iv: wallet.iv.toString('base64')
+    }
+    this.post('/wallet', wallet, cb)
+  }
+  submitTx (tx, cb) {
+    tx.txid = tx.txid.toString('base64')
+    this.post('/transaction', tx, cb)
+  }
+}
+
+module.exports = old(Client)
