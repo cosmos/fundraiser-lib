eec8c1f780e5ae3e1bb153de0f61e678f8bacece
Matt Bell <mappum@gmail.com>
2017-03-04 23:45:32 -0800

    Refactored bitcoin code to use Blockchain.info API instead of Blockr.io

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -1,120 +1,106 @@
 const bs58check = require('bs58check')
-const request = require('request')
-const { Transaction, script, address, networks } = require('bitcoinjs-lib')
+const { Transaction, script, address } = require('bitcoinjs-lib')
+const bci = require('blockchain.info/blockexplorer')
+bci.pushTx = require('blockchain.info/pushtx').pushtx
 const secp256k1 = require('secp256k1')
+const reverse = require('buffer-reverse')
 const { sha2, ripemd160 } = require('./hash.js')
 const { byte, concat } = require('./util.js')
 
-const EXODUS_ADDRESS = 'mvPBr7EqiAn41Hqs9Du8ovNFKveU1sdecX'
+const DEV = process.env.NODE_ENV === 'development'
+const EXODUS_ADDRESS = '1EaV33reN8XWWUfs5jkbGMD399vie5KQc4'
 const FEE_RATE = 220 // satoshis per byte
-const MINIMUM_AMOUNT = 1000000 // min satoshis to send to exodus
+const MINIMUM_AMOUNT = DEV ? 60000 : 1000000 // min satoshis to send to exodus
 const ATOMS_PER_BTC = 2000
 const MINIMUM_OUTPUT = 1000
 
 // exodus pubkey hash
+// TODO: exodus should be P2Sh
+// TODO: address prefix byte sanity check
 const exodusPkh = bs58check.decode(EXODUS_ADDRESS).slice(1)
 
-function getAddress (pub, testnet = false) {
-  let network = testnet ? networks.testnet : networks.bitcoin
-  let outputScript = script.pubKeyHashOutput(ripemd160(sha2(pub)))
-  return address.fromOutputScript(outputScript, network)
+function getAddress (pub) {
+  let pubkeyHash = ripemd160(sha2(pub))
+  let outputScript = script.pubKeyHashOutput(pubkeyHash)
+  return address.fromOutputScript(outputScript)
 }
 
-function blockrRequest (method, url, { testnet }, data, cb) {
-  let network = testnet ? 'tbtc' : 'btc'
-  return request({
-    method,
-    url: `https://${network}.blockr.io/api/v1/${url}`,
-    json: data || true
-  }, (err, res, body) => {
+// call a callback from a Promise
+function cbify (promise, cb) {
+  promise
+    .then((res) => cb(null, res))
+    .catch((err) => cb(err))
+}
+
+// fetch all utxos for this address
+function fetchUtxos (address, cb) {
+  cbify(bci.getAddress(address), (err, res) => {
+    // results from bc.i are paginated and we are only
+    // getting the first page, so we're assuming nobody is
+    // going to send 50+ txs
     if (err) return cb(err)
-    if (body.status !== 'success') {
-      return cb(Error(body.message))
+    let utxos = []
+    let amount = 0
+    for (let tx of res.txs) {
+      for (let output of tx.out) {
+        if (output.spent) continue
+        if (output.addr !== address) continue
+        amount += output.value
+        output.txid = tx.hash
+        utxos.push(output)
+      }
     }
-    cb(null, body.data)
+    cb(null, { utxos, amount })
   })
 }
 
-function fetchUnspent (address, opts, cb) {
-  blockrRequest('GET', `address/unspent/${address}?unconfirmed=1`, opts, null, cb)
-}
-
-function fetchTx (hash, opts, cb) {
-  blockrRequest('GET', `tx/raw/${hash}`, opts, null, (err, res) => {
-    if (err) return cb(err)
-    cb(null, Transaction.fromHex(res.tx.hex))
-  })
-}
-
-function waitForTx (address, opts, cb) {
+function waitForPayment (address, cb) {
   const done = (err, res) => {
     clearInterval(interval)
     cb(err, res)
   }
   const checkForUnspent = () => {
-    fetchUnspent(address, opts, (err, res) => {
+    fetchUtxos(address, (err, res) => {
       if (err) return done(err)
-      if (res.unspent.length === 0) return
-      // TODO: handle multiple txs?
-      fetchTx(res.unspent[0].tx, opts, (err, tx) => {
-        if (err) return done(err)
-        let info = getTxInfo(tx, address)
-        done(null, info)
-      })
+      if (res.amount < MINIMUM_AMOUNT) return
+      done(null, res)
     })
   }
-  let interval = setInterval(checkForUnspent, 5000)
+  // poll once every 6 seconds
+  let interval = setInterval(checkForUnspent, 6000)
   checkForUnspent()
 }
 
-// check if an output has a P2PKH script and pays to the given address
-function isSpendable (output, address) {
-  let isPkh = script.classifyOutput(output.script) === 'pubkeyhash'
-  let pkh = bs58check.decode(address).slice(1)
-  let key = Buffer(script.decompile(output.script)[2])
-  let paysToAddress = key.equals(pkh)
-  return isPkh && paysToAddress
+function pushTx (tx, cb) {
+  cbify(bci.pushTx(tx.toHex()), cb)
 }
 
-// gets info about a transaction sent to an intermediate address
-function getTxInfo (tx, address) {
-  let amount = 0
-  let outputs = tx.outs.filter((out) => isSpendable(out, address))
-  for (let output of outputs) {
-    output.index = tx.outs.indexOf(output)
-    amount += output.value
-  }
-  return { tx, outputs, amount }
-}
-
-function pushTx (tx, opts, cb) {
-  let txJson = { hex: tx.toHex() }
-  blockrRequest('POST', `tx/push`, opts, txJson, cb)
-}
-
-function createFinalTx (wallet, intermediateTx) {
-  if (intermediateTx.amount < MINIMUM_AMOUNT) {
+function createFinalTx (wallet, inputs) {
+  if (inputs.amount < MINIMUM_AMOUNT) {
     throw Error(`Intermediate tx is smaller than minimum.
       minimum=${MINIMUM_AMOUNT}
-      actual=${intermediateTx.amount}`)
+      actual=${inputs.amount}`)
   }
 
   let tx = new Transaction()
 
   // add inputs from intermediate tx
-  let intermediateHash = intermediateTx.tx.getHash()
-  for (let output of intermediateTx.outputs) {
-    tx.addInput(intermediateHash, output.index)
+  let inputMap = {}
+  for (let output of inputs.utxos) {
+    let txid = reverse(Buffer(output.txid, 'hex'))
+    tx.addInput(txid, output.n)
+    inputMap[`${output.txid}:${output.n}`] = output
   }
 
   // pay to exodus address, spendable by Cosmos developers
+  // TODO: exodus should be P2SH
   let payToExodus = script.pubKeyHashOutput(exodusPkh)
-  tx.addOutput(payToExodus, intermediateTx.amount)
+  tx.addOutput(payToExodus, inputs.amount)
 
-  // OP_RETURN data output to specify user's Cosmos address
-  // this output has a value of 0
-  let specifyCosmosAddress = script.nullDataOutput(Buffer(wallet.addresses.cosmos, 'hex'))
-  tx.addOutput(specifyCosmosAddress, 0)
+  // output to specify user's Cosmos address
+  let cosmosAddress = Buffer(wallet.addresses.cosmos, 'hex')
+  let specifyCosmosAddress = script.pubKeyHashOutput(cosmosAddress)
+  tx.addOutput(specifyCosmosAddress, MINIMUM_OUTPUT)
 
   // deduct fee from exodus output
   let feeAmount = tx.byteLength() * FEE_RATE
@@ -130,17 +116,18 @@ function createFinalTx (wallet, intermediateTx) {
   // sign inputs
   let privKey = wallet.privateKeys.bitcoin
   let pubKey = wallet.publicKeys.bitcoin
+  let sigHashType = Transaction.SIGHASH_ALL
   for (let i = 0; i < tx.ins.length; i++) {
-    let input = tx.ins[i]
-    let prevOut = intermediateTx.tx.outs[input.index]
-    let sigHashType = Transaction.SIGHASH_ALL
-    let sigHash = tx.hashForSignature(i, prevOut.script, sigHashType)
+    let input = tx.ins[i] // tx input
+    let prevOut = inputs.utxos[i] // utxo associated w/ this input
+    let scriptPubKey = Buffer(prevOut.script, 'hex') // utxo's script
+    let sigHash = tx.hashForSignature(i, scriptPubKey, sigHashType)
     let signature = sign(privKey, sigHash)
     signature = concat(signature, byte(sigHashType)) // append sighash type byte
     input.script = script.pubKeyHashInput(signature, pubKey)
   }
 
-  let paidAmount = intermediateTx.amount
+  let paidAmount = inputs.amount
   let atomAmount = (tx.outs[0].value * ATOMS_PER_BTC) / 1e8
   return { tx, paidAmount, feeAmount, atomAmount }
 }
@@ -153,10 +140,9 @@ function sign (privKey, sigHash) {
 
 module.exports = {
   getAddress,
-  fetchUnspent,
-  fetchTx,
+  fetchUtxos,
   pushTx,
-  waitForTx,
+  waitForPayment,
   createFinalTx,
   MINIMUM_AMOUNT,
   ATOMS_PER_BTC
