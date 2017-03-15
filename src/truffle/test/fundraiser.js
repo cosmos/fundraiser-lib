952cc6bd1d8f3e60d5bb2e6c829c4c1bf8b0fa50
Ethan Buchman <ethan@coinculture.info>
2017-03-15 16:41:03 -0400

    truffle: fix web3 location

--- src/truffle/test/fundraiser.js
+++ src/truffle/test/fundraiser.js
@@ -2,7 +2,7 @@ const leftPad = require('left-pad');
 const xor = require('bitwise-xor');
 
 
-var Web3 = require('../../node_modules/web3');
+var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
