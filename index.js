d163b87f2b5676db29d97dac01f7086838c339d5
Matt Bell <mappum@gmail.com>
2017-03-16 17:32:34 -0700

    Export ethereum module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = Object.assign(
   {
     bitcoin: require('./lib/bitcoin.js'),
+    ethereum: require('./lib/ethereum.js'),
     sendEmail: require('./lib/backups.js')
   },
   require('./lib/wallet.js')
