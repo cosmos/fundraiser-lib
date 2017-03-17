f459a762bd40ca1f7c4d112fc7fd48413ceb1c1f
Matt Bell <mappum@gmail.com>
2017-03-17 13:01:14 -0700

    Made eth code adhere to standard js linting rules

--- src/ethereum.js
+++ src/ethereum.js
@@ -43,12 +43,12 @@ var abi = [{
 let MyContract = web3.eth.contract(abi)
 var contractInstance = MyContract.at('0x00')
 
-function getTransaction(cosmosAddr, ethAddr) {
-	return {
-		to: FUNDRAISER_CONTRACT,
-		gas: GAS_LIMIT,
-		data: getTransactionData(cosmosAddr,ethAddr)
-	}
+function getTransaction (cosmosAddr, ethAddr) {
+  return {
+    to: FUNDRAISER_CONTRACT,
+    gas: GAS_LIMIT,
+    data: getTransactionData(cosmosAddr, ethAddr)
+  }
 }
 
 // TODO: make sure the addresses aren't empty
@@ -65,63 +65,70 @@ function getTransactionData (cosmosAddr, ethAddr) {
   return contractInstance.donate.getData(cosmosAddr, ethAddr, checksum)
 }
 
-
-
-function esiRequest (method, url, data, cb) {
+function esiRequest (url, qs, cb) {
   return request({
-    method,
     url: `https://api.etherscan.io/${url}`,
-    qs: { cors: true },
-    form: data
+    qs,
+    json: true
   }, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
+    if (err || res.statusCode !== 200 || body.result === 0) {
       return cb(err || Error(res.statusCode), body)
     }
-    try {
-      body = JSON.parse(body)
-    } catch (err) {}
     cb(null, body)
   })
 }
 
 // fetch the current atomRate
-function fetchAtomRate(address, cb) {
-  var txData = web3.sha3("atomRate()").slice(0, 10); // "0xf5c96216" 
-  var method = "api?module=proxy&action=eth_call"
-  esiRequest('GET', `${method}&to=${address}&data=${txData}&tag=latest`,null, (err, res) => {
+function fetchAtomRate (address, cb) {
+  let txData = web3.sha3('atomRate()').slice(0, 10) // '0xf5c96216'
+  esiRequest('api?module=proxy&action=eth_call', {
+    to: address,
+    data: txData,
+    tag: 'latest'
+  }, (err, res) => {
     if (err) return cb(err)
-    cb(null, parseInt(res.result, 16));
+    cb(null, parseInt(res.result, 16))
   })
 }
 
 // fetch the total raised and total atoms
-function fetchTotals(address, cb) {
-  var txData1 = web3.sha3("totalAtom()").slice(0, 10); 
-  var txData2 = web3.sha3("totalEther()").slice(0, 10); 
-  var method = "api?module=proxy&action=eth_call";
-  var divisor = Math.pow(10,18);
+function fetchTotals (address, cb) {
+  let txData1 = web3.sha3('totalAtom()').slice(0, 10)
+  let txData2 = web3.sha3('totalEther()').slice(0, 10)
+  let divisor = 1e18
 
-  esiRequest('GET', `${method}&to=${address}&data=${txData1}&tag=latest`,null, (err, res1) => {
-    if (err) return cb(err);
-
-    totalAtoms = parseInt(res1.result, 16) / divisor;
-    esiRequest('GET', `${method}&to=${address}&data=${txData2}&tag=latest`,null, (err, res2) => {
-      if (err) return cb(err);
-      totalEther= parseInt(res2.result, 16) / divisor;
-      cb(null, {ether: totalEther, atoms: totalAtoms});
+  let ethCall = 'api?module=proxy&action=eth_call'
+  esiRequest(ethCall, {
+    to: address,
+    data: txData1,
+    tag: 'latest'
+  }, (err, res1) => {
+    if (err) return cb(err)
+    let atoms = parseInt(res1.result, 16) / divisor
+    esiRequest(ethCall, {
+      to: address,
+      data: txData2,
+      tag: 'latest'
+    }, (err, res2) => {
+      if (err) return cb(err)
+      let ether = parseInt(res2.result, 16) / divisor
+      cb(null, { ether, atoms })
     })
   })
 }
 
 // TODO: limit so it doesn't fetch all txs
-function fetchTxs(address, cb) {
-  var url = `api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc`
-  esiRequest('GET', url, null, (err, res) => {
-    if (err) return cb(err);
+function fetchTxs (address, cb) {
+  esiRequest('api?module=account&action=txlist', {
+    address,
+    startblock: 0,
+    endblock: 99999999,
+    sort: 'desc'
+  }, (err, res) => {
+    if (err) return cb(err)
     cb(null, res.result)
-  });
+  })
 }
-
 
 module.exports = {
   getAddress,
@@ -132,5 +139,3 @@ module.exports = {
   fetchTxs,
   ATOMS_PER_ETH
 }
-
-
